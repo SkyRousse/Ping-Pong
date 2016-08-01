e2c16678beb2ee58c38c41f54a007a0e354fc8e6
sr <skyrousse@gmail.com>
2016-07-31 23:24:34 -0700

    add form to collect selected skin

---
+++ js/skin-interface.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $('#skin').submit(function(event) {
+    event.preventDefault();
+    var skin = $('#skin option:selected').text();
+    // $('#skin').hide();
+    $('#skin-output').prepend('<p>Your skin is:,' + skin + '</p>')
+  });
+});
